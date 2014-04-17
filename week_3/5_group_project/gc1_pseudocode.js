427a0353b31521e1332ea3cd6ae5d707919a6deb
BenBrostoff <ben.brostoff@gmail.com>
2014-04-17 16:11:41 -0400

    Update gc1_pseudocode.js

--- week_3/5_group_project/gc1_pseudocode.js
+++ week_3/5_group_project/gc1_pseudocode.js
@@ -1,4 +1,4 @@
-receive input that is a group of numbers
-return the sum of the group of numbers
-return the mean of the group of numbers
-return the median of the group of numbers
+//receive input that is a group of numbers
+//return the sum of the group of numbers
+//return the mean of the group of numbers
+//return the median of the group of numbers
